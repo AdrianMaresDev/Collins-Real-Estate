32ae7458ce750bb475eb8cffc2afcee2990f082a
AdrianMares <adrianmares.dev@gmail.com>
2024-12-10 01:51:03 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Montserrat", "sans-serif"]
+      },
+      colors: {
+        "off-white": "#F5F4F2",
+        "dark-yellow": "#EDAB1E"
+      },
+    },
+  },
+  plugins: [],
+}
+
