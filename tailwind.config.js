abd482e9cc5d98de9b1210efb59996828ddd9605
AdrianMares <adrianmares.dev@gmail.com>
2024-12-14 22:07:29 -0500

    added nav hover animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        custom: ["Montserrat", "sans-serif"],
         poppins: ["Poppins", "sans-serif"]
-      },
-      colors: {
-        "off-white": "#F5F4F2",
-        "dark-yellow": "#EDAB1E"
       },
     },
   },
